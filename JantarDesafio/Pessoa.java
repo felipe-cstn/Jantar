d45b0b9fc45ffed052f8935dbb6e76e88dae3f80
Felipe Celestino <84358544+felipe-cstn@users.noreply.github.com>
2023-09-22 16:38:08 -0300

    Update Pessoa.java
    Pessoa, Final

--- JantarDesafio/Pessoa.java
+++ JantarDesafio/Pessoa.java
@@ -2,17 +2,24 @@ package JantarDesafio;
 
 public class Pessoa {
 	
-	String nome;
-	double peso;
-	
-	Pessoa(String nomePessoa, double pesoPessoa){
-		nome = nomePessoa;
-		peso = pesoPessoa;
-	}
-	
-	
-	
-	public void comer() {
-		peso
-	}
+	 String nome;
+		double peso;
+		double pesoComida;
+		
+		Pessoa(String nomePessoa, double pesoPessoa, double pesoComida){
+			nome = nomePessoa;
+			peso = pesoPessoa;
+			this.pesoComida = pesoComida;
+		}
+		
+		
+		public double comer() {
+			double pesoFinal = this.pesoComida + peso;
+			return pesoFinal;
+		}
+		
+		public double cagar() {
+			double pesoFinal = peso - this.pesoComida;
+			return pesoFinal;
+		}
 }
